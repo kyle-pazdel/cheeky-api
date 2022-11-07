2a4358031ccb14ffc5299e9fc69647d506e9a8b4
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-07 11:14:10 -0800

    Adds validations and associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  include ImageUploader::Attachment(:image)
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true, uniqueness: true
+  has_many :bookings
+  has_many :performers, through: :bookings
 end
