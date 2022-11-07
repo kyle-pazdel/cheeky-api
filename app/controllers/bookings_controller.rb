b85246026a09d79ef36f97a6c4a6e3fb7649c167
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-07 13:36:25 -0800

    Adds bookings index view to views and index action to bookings_controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all
 
-    render json: @bookings
+    render template: "bookings/index"
   end
 
   # GET /bookings/1
@@ -39,13 +39,14 @@ class BookingsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_booking
-      @booking = Booking.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def booking_params
-      params.fetch(:booking, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_params
+    params.fetch(:booking, {})
+  end
 end
