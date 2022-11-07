b9adb7b422cd68d83e51c707ae0499daf9d6baa4
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-07 14:27:13 -0800

    Adds index and show json.jb files to views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,7 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Review.all
 
-    render json: @reviews
+    render template: "index/reviews"
   end
 
   # GET /reviews/1
@@ -39,13 +39,14 @@ class ReviewsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = Review.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def review_params
-      params.fetch(:review, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def review_params
+    params.fetch(:review, {})
+  end
 end
