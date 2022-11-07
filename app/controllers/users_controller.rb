5f4e99f7b2417bf94cada19b2c33400165c2678f
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-07 12:34:53 -0800

    Adds update method to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,11 +27,15 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    @user.first_name = if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
+    @user.first_name = params[:first_name] || @user.first_name
+    @user.last_name = params[:last_name] || @user.last_name
+    @user.phone_number = params[:phone_number] || @user.phone_number
+    @user.email = params[:email] || @user.email
+    if @user.save
+      render template: "users/show"
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   # DELETE /users/1
