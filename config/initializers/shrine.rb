8d95946b8648109f01ba02aa407639f9753bebb0
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-08 08:37:43 -0800

    Configures Shrine gem for future image uploads to user instances

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -12,3 +12,8 @@ else
     store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
   }
 end
+Shrine.plugin :activerecord    # loads Active Record integration
+Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
+Shrine.plugin :restore_cached_data  # extracts metadata for assigned cached files
+Shrine.plugin :validation_helpers
+Shrine.plugin :validation
