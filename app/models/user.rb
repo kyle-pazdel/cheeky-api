9aafa52970a01e7225867aff11f7f367b7aa5f60
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-11 08:19:11 -0800

    updates user:review associations for more dynamic rendering on front end

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :phone_number, presence: true, uniqueness: true
   has_many :bookings
   has_many :performers, through: :bookings
+  has_many :reviews, through: :bookings
 end
