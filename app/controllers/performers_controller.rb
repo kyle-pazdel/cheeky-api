b3931594fc7d2e6d7cd8ddd3cf692e0362053740
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-11 17:39:58 -0800

    Adds performer update method to performers_controller

--- app/controllers/performers_controller.rb
+++ app/controllers/performers_controller.rb
@@ -26,10 +26,26 @@ class PerformersController < ApplicationController
 
   # PATCH/PUT /performers/1
   def update
-    if @performer.update(performer_params)
-      render json: @performer
+    if @performer.user.id == current_user.id
+      @performer.name = params[:name] || @performer.name
+      @performer.phone_number = params[:phone_number] || @performer.phone_number
+      @performer.email = params[:email] || @performer.email
+      @performer.shortest_gig = params[:shortest_gig] || @performer.shortest_gig
+      @performer.longest_gig = params[:longest_gig] || @performer.longest_gig
+      @performer.city = params[:city] || @performer.city
+      @performer.state = params[:state] || @performer.state
+      @performer.rate = params[:rate] || @performer.rate
+      @performer.bio = params[:bio] || @performer.bio
+      @performer.instagram_handle = params[:instagram_handle] || @performer.instagram_handle
+      @performer.twitter_handle = params[:twitter_handle] || @performer.twitter_handle
+      @performer.performance_type = params[:performance_type] || @performer.performance_type
+      if @performer.save
+        render template: "performers/show"
+      else
+        render json: { errors: @performer.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: @performer.errors, status: :unprocessable_entity
+      render json: { message: "Please log in to update account details." }, status: :unauthorized
     end
   end
 
