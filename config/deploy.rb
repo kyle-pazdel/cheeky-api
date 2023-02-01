2e0bd1bc4b6240f2818724400e94e9058af145d5
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-31 21:07:44 -0800

    Revert "Adds secret to config production"
    This reverts commit 21d55b589d0e815165c07f5dccd3d33565d470ec.

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,9 +36,6 @@ set :keep_releases, 5
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", 'config/master.key'
-append :linked_files,  'config/credentials/production.key'
-
-
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
