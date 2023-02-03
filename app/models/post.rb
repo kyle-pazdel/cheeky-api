32854cbd02955bcdc466f212aa70632582f30e8c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-03 08:36:23 -0800

    Adds file validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   belongs_to :performer
+  validates :image, presence: true, blob: { content_type: [ 'image/jpg', 'image/jpeg'], size_range: 0..(0.1.megabytes) }
 
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
