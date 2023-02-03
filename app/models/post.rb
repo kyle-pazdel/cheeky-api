3df251d2729e2f4aae09ec8246b271170092f0ca
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-02-03 08:39:02 -0800

    Removes post image validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   belongs_to :performer
-  validates :image, presence: true, blob: { content_type: [ 'image/jpg', 'image/jpeg'], size_range: 0..(0.1.megabytes) }
 
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
